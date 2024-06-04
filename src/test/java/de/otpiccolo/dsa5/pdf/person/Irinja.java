f75e543080d3118145fdd48d94661d5b6e352107
OT Piccolo <OT_Piccolo@web.de>
2024-06-04 23:37:06 +0200

    Added Irinja.

---
+++ src/test/java/de/otpiccolo/dsa5/pdf/person/Irinja.java
@@ -0,0 +1,74 @@
+package de.otpiccolo.dsa5.pdf.person;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+import de.otpiccolo.dsa5.data.kampfsonderfertigkeiten.KampfsonderfertigkeitReader;
+import de.otpiccolo.dsa5.data.kampfsonderfertigkeiten.KampfsonderfertigkeitWriter;
+import de.otpiccolo.dsa5.data.nachteile.NachteilReader;
+import de.otpiccolo.dsa5.data.nachteile.NachteilWriter;
+import de.otpiccolo.dsa5.data.vorteile.VorteilReader;
+import de.otpiccolo.dsa5.data.vorteile.VorteilWriter;
+import de.otpiccolo.dsa5.data.zauber.ZauberReader;
+import de.otpiccolo.dsa5.data.zauber.ZauberWriter;
+import de.otpiccolo.dsa5.pdf.data.image.ImageReader;
+import de.otpiccolo.dsa5.pdf.data.image.ImageWriter;
+import de.otpiccolo.dsa5.pdf.data.paragraph.ParagraphData;
+import de.otpiccolo.dsa5.pdf.data.paragraph.ParagraphWriter;
+import de.otpiccolo.dsa5.pdf.page.DefaultPage;
+import de.otpiccolo.dsa5.pdf.page.IPage;
+import de.otpiccolo.dsa5.pdf.page.predefined.SchicksalspunktePage;
+import de.otpiccolo.dsa5.pdf.page.predefined.ZauberModPage;
+
+/**
+ * Information about Irinja.
+ */
+public class Irinja extends Person {
+
+	/**
+	 * Constructor.
+	 */
+	public Irinja() {
+		final DefaultPage vorteilNachteilPage = new DefaultPage("Vorteile & Nachteile");
+		vorteilNachteilPage.getWriters().add(fillWriter(VorteilWriter::new, VorteilReader::new, "Begabung", "Beidhändig", "Nichtschläfer (*)", "Schlangenmensch", "Waffenbegabung", "Zauberer"));
+		vorteilNachteilPage.getWriters().add(fillWriter(NachteilWriter::new, NachteilReader::new, "Körperliche Auffälligkeit", "Niedrige Zähigkeit"));
+
+		final DefaultPage kampfPage = new DefaultPage("Kampfsonderfertigkeiten");
+		kampfPage.getWriters().add(fillWriter(KampfsonderfertigkeitWriter::new, KampfsonderfertigkeitReader::new, "Beidhändiger Kampf I-II", "Finte I-III", "Präziser Schuss/Wurf I-III", "Präziser Stich I-III", "Schnellziehen", "Unterlaufen I-II", "Verbessertes Ausweichen I-III"));
+
+		final DefaultPage zauberPage = new DefaultPage("Zaubersprüche");
+		zauberPage.getWriters().add(fillWriter(ZauberWriter::new, ZauberReader::new, "Axxeleratus", "Chamaelioni", "Foramen"));
+
+		final IPage otherPage = getOtherPage();
+		final IPage modPage = new ZauberModPage();
+		final IPage schipsPage = new SchicksalspunktePage();
+
+		final DefaultPage imagePage = new DefaultPage();
+		imagePage.getWriters().add(new ImageWriter(new ImageReader().readData("D:\\RP\\Bilder\\Irinja.png")));
+
+		setPages(Stream.of(vorteilNachteilPage, kampfPage, zauberPage, otherPage, modPage, schipsPage, imagePage));
+	}
+
+	private IPage getOtherPage() {
+		final DefaultPage page = new DefaultPage("Sonstiges");
+
+		final ParagraphData father = new ParagraphData("Vater: Andrej Itolojeff.");
+		final ParagraphData circus = new ParagraphData("Zirkus: Wandernder Hut, geleitet von der Großartigen und Mächtigen Trixxxxxxie!");
+		final List<ParagraphData> family = Arrays.asList(father, circus);
+		page.getWriters().add(new ParagraphWriter("Familie", family));
+
+		final ParagraphData boneDagger = new ParagraphData("Knochendolch: +4 Schaden auf Meucheln.");
+		final ParagraphData discDagger = new ParagraphData("Scheibendolch: Gegner mit RK >= 4 erhalten -2 auf RK und +1 Schaden. Kein Kreuzblock möglich.");
+		final List<ParagraphData> weapons = Arrays.asList(boneDagger, discDagger);
+		page.getWriters().add(new ParagraphWriter("Waffen", weapons));
+
+		final ParagraphData butteflyAmulet = new ParagraphData("Schmetterlings-Amulett: Kann 1/Tag Saturias Herrlichkeit zaubern als QS1 für 3 Stunden.");
+		final ParagraphData baronRing = new ParagraphData("Ring des Barons: Erhält den Vorteil 'Nichtschläfer', aber auch -1 LP Regeneration. Untote ignorieren den Träger, solange der Träger nicht aktiv gegen die Untoten vorgeht. Beim Abziehen des Rings gibt es 3W6 Schaden.");
+		final List<ParagraphData> items = Arrays.asList(butteflyAmulet, baronRing);
+		page.getWriters().add(new ParagraphWriter("Gegenstände", items));
+
+		return page;
+	}
+
+}
--- src/test/java/ut/de/otpiccolo/dsa5/pdf/person/PersonTest.java
+++ src/test/java/ut/de/otpiccolo/dsa5/pdf/person/PersonTest.java
@@ -6,11 +6,26 @@ import java.io.File;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
+import de.otpiccolo.dsa5.pdf.person.Irinja;
 import de.otpiccolo.dsa5.pdf.person.Lothar;
 import de.otpiccolo.dsa5.pdf.person.Thyra;
 
 @SuppressWarnings("javadoc")
 public class PersonTest {
+
+	@Test
+	@Disabled
+	public void writeIrinja() throws Exception {
+		final String source = "D:\\Schieb\\Irinja.pdf";
+		final String destination = "C:\\Users\\OT Piccolo\\Desktop\\temp\\test.pdf";
+
+		final Irinja irinja = new Irinja();
+		irinja.setSource(source);
+		irinja.setDestination(destination);
+		irinja.writeDocument();
+
+		Desktop.getDesktop().open(new File(destination));
+	}
 
 	@Test
 	@Disabled
