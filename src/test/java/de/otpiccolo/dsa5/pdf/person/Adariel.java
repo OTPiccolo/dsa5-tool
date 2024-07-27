2a1ee8dfc795af4ded8ee93343676bce2e47b260
OT Piccolo <OT_Piccolo@web.de>
2024-07-27 23:29:41 +0200

    Updated Adariel.

--- src/test/java/de/otpiccolo/dsa5/pdf/person/Adariel.java
+++ src/test/java/de/otpiccolo/dsa5/pdf/person/Adariel.java
@@ -2,12 +2,12 @@ package de.otpiccolo.dsa5.pdf.person;
 
 import java.util.stream.Stream;
 
+import de.otpiccolo.dsa5.data.allgemeinesonderfertigkeiten.AllgemeinesonderfertigkeitReader;
+import de.otpiccolo.dsa5.data.allgemeinesonderfertigkeiten.AllgemeinesonderfertigkeitWriter;
 import de.otpiccolo.dsa5.data.elfenlied.ElfenliedReader;
 import de.otpiccolo.dsa5.data.elfenlied.ElfenliedWriter;
 import de.otpiccolo.dsa5.data.elixiere.ElixierReader;
 import de.otpiccolo.dsa5.data.elixiere.ElixierWriter;
-import de.otpiccolo.dsa5.data.kampfsonderfertigkeiten.KampfsonderfertigkeitReader;
-import de.otpiccolo.dsa5.data.kampfsonderfertigkeiten.KampfsonderfertigkeitWriter;
 import de.otpiccolo.dsa5.data.nachteile.NachteilReader;
 import de.otpiccolo.dsa5.data.nachteile.NachteilWriter;
 import de.otpiccolo.dsa5.data.vorteile.VorteilReader;
@@ -36,19 +36,17 @@ public class Adariel extends Person {
 		vorteilNachteilPage.getWriters().add(fillWriter(VorteilWriter::new, VorteilReader::new, "Altersresistenz (*)", "Dunkelsicht I-II (*)", "Nichtschläfer (*)", "Verbesserte Regeneration (Astralenergie) I-III", "Zweistimmiger Gesang (*)"));
 		vorteilNachteilPage.getWriters().add(fillWriter(NachteilWriter::new, NachteilReader::new, "Körpergebundene Kraft", "Sensibler Geruchssinn (*)", "Unfähig", "Unverträglichkeit gegenüber Alkohol", "Wahrer Name"));
 
-		final DefaultPage kampfPage = new DefaultPage("Kampfsonderfertigkeiten");
-		kampfPage.getWriters().add(fillWriter(KampfsonderfertigkeitWriter::new, KampfsonderfertigkeitReader::new, "Belastungsgewöhnung I-II", "Schnellladen (Kampftechnik)"));
+		final DefaultPage kampfPage = new DefaultPage("Sonderfertigkeiten");
+		kampfPage.getWriters().add(fillWriter(AllgemeinesonderfertigkeitWriter::new, AllgemeinesonderfertigkeitReader::new, "Fertigkeitsspezialisierung (Talente)", "Geländekunde", "Ortskenntnis"));
 
-		final DefaultPage zauberPage1 = new DefaultPage("Zaubersprüche");
+		final DefaultPage zauberPage1 = new DefaultPage("Zauber");
 		zauberPage1.getWriters().add(fillWriter(ZauberWriter::new, ZauberReader::new, "Axxeleratus", "Balsam Salabunde", "Bannbaladin", "Hilfreiche Pfote"));
 		final DefaultPage zauberPage2 = new DefaultPage();
 		zauberPage2.getWriters().add(fillWriter(ZauberWriter::new, ZauberReader::new, "Humuspfeil", "Spurlos", "Tiergedanken"));
 
-		final DefaultPage zaubertrickPage = new DefaultPage("Zaubertricks");
-		zaubertrickPage.getWriters().add(fillWriter(ZaubertrickWriter::new, ZaubertrickReader::new, "Blütenduft", "Elfenhaar", "Putziges Tierchen", "Schmutzabweisend"));
-
-		final DefaultPage elfenliedPage = new DefaultPage("Elfenlieder");
-		elfenliedPage.getWriters().add(fillWriter(ElfenliedWriter::new, ElfenliedReader::new, "Friedenslied", "Lied der Reinheit"));
+		final DefaultPage sonstigeZauberPage = new DefaultPage("Zaubertricks & Elfenlieder");
+		sonstigeZauberPage.getWriters().add(fillWriter(ZaubertrickWriter::new, ZaubertrickReader::new, "Blütenduft", "Elfenhaar", "Putziges Tierchen", "Schmutzabweisend"));
+		sonstigeZauberPage.getWriters().add(fillWriter(ElfenliedWriter::new, ElfenliedReader::new, "Friedenslied", "Lied der Reinheit"));
 
 		final DefaultPage itemPage = new DefaultPage("Gegenstände");
 		itemPage.getWriters().add(fillWriter(ElixierWriter::new, ElixierReader::new, "Heiltrank", "Zaubertrank"));
@@ -59,7 +57,7 @@ public class Adariel extends Person {
 		final DefaultPage imagePage = new DefaultPage();
 		imagePage.getWriters().add(new ImageWriter(new ImageReader().readData("D:\\RP\\Bilder\\Adariel Abendfreundin.png")));
 
-		setPages(Stream.of(vorteilNachteilPage, kampfPage, zauberPage1, zauberPage2, zaubertrickPage, elfenliedPage, itemPage, modPage, schipsPage, imagePage));
+		setPages(Stream.of(vorteilNachteilPage, kampfPage, zauberPage1, zauberPage2, sonstigeZauberPage, itemPage, modPage, schipsPage, imagePage));
 	}
 
 }
