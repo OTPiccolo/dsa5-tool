f792c48f15407781ea296470fdf1efcec9a8e054
OT Piccolo <OT_Piccolo@web.de>
2024-11-15 00:44:52 +0100

    Added reader/writer for Waffenvorteile and -nachteile for Weapons.

---
+++ src/main/java/de/otpiccolo/dsa5/data/weapon/swords/SchwertWriter.java
@@ -0,0 +1,48 @@
+package de.otpiccolo.dsa5.data.weapon.swords;
+
+import java.io.IOException;
+import java.util.Collection;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.PDPageContentStream.AppendMode;
+import org.apache.pdfbox.pdmodel.common.PDRectangle;
+
+import de.otpiccolo.dsa5.pdf.data.ADataWriter;
+import de.otpiccolo.pdf.PDUtil;
+
+/**
+ * Class to write Schwerter to a document.
+ */
+public class SchwertWriter extends ADataWriter {
+
+	private final Collection<SchwertData> data;
+
+	/**
+	 * A collection containing Schwerter.
+	 *
+	 * @param data
+	 *            The Schwerter.
+	 */
+	public SchwertWriter(final Collection<SchwertData> data) {
+		this.data = data;
+	}
+
+	@Override
+	public PDRectangle writeData(final PDDocument doc, final PDPage page, final PDRectangle availableSpace) throws IOException {
+		PDRectangle space = PDUtil.copyRectangle(availableSpace);
+		try (PDPageContentStream content = new PDPageContentStream(doc, page, AppendMode.APPEND, true, true)) {
+			for (final SchwertData schwert : data) {
+				space = writeTitle(schwert.name(), content, space, 5f);
+				space = writeParagraph("Vorteil: " + schwert.upside(), content, space, 5f);
+				space = writeParagraph("Nachteil: " + schwert.downside(), content, space, 15f);
+			}
+			// Remove last Schwert spacing.
+			space.setUpperRightY(space.getUpperRightY() + 15f);
+			drawRectangle(content, space, availableSpace);
+		}
+		return space;
+	}
+
+}
---
+++ src/test/java/ut/de/otpiccolo/dsa5/data/UlissesWeaponReaderTest.java
@@ -0,0 +1,25 @@
+package ut.de.otpiccolo.dsa5.data;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import de.otpiccolo.dsa5.data.weapon.swords.SchwertData;
+import de.otpiccolo.dsa5.data.weapon.swords.SchwertReader;
+
+/**
+ * Test to make sure that all Weapon Reader correctly read data from Ulisses
+ * pages and they didn't change how the information is posted on their pages.
+ */
+@SuppressWarnings("javadoc")
+public class UlissesWeaponReaderTest {
+
+	@Test
+	public void testSchwerter() {
+		final var reader = new SchwertReader();
+		final var data = reader.readData("Säbel");
+
+		final var expected = new SchwertData("Säbel", "Von einem Reittier aus geführt, bekommt der Träger des Säbels einen Bonus von +1 TP.", "Gegen Rüstungen mit RS 4 oder höher richtet die Waffe –1 TP an.");
+		Assertions.assertEquals(expected, data);
+	}
+
+}
