bcc08a9876b11f62197a9b6e84bb3dddcbb04767
OT Piccolo <OT_Piccolo@web.de>
2025-03-08 03:22:10 +0100

    Updated ZauberModPage.

--- src/main/java/de/otpiccolo/dsa5/pdf/page/predefined/ZauberModPage.java
+++ src/main/java/de/otpiccolo/dsa5/pdf/page/predefined/ZauberModPage.java
@@ -23,8 +23,9 @@ public class ZauberModPage extends DefaultPage {
 		final ParagraphData pg4 = new ParagraphData("Zauberdauer erhöhen: Zauberdauer um eine Stufe erhöht, Zauberprobe um 1 erleichtert.");
 		final ParagraphData pg5 = new ParagraphData("Zauberdauer senken: Zauberdauer um eine Stufe gesenkt, Zauberprobe um 1 erschwert.");
 		final ParagraphData pg6 = new ParagraphData("Geste oder Formel weglassen: Zauberprobe um jeweils 2 erschwert.");
-		final ParagraphData pg7 = new ParagraphData("Emotionen (nur Hexe): Unter dem Einfluss zum Zauber passender Gefühle kann die Hexe eine Erleichterung von bis zu 2 für die Zauberprobe erhalten. Werden ihre Gefühle künstlich gedämpft, zum Beispiel durch Ilmenblatt, oder zaubert sie entgegen ihrer Gefühle, erschwert dies die Proben um bis zu 2. Die Hexe kann sich gezielt in ihre Emotionen hineinsteigern, sodass sie innerhalb von zwei Kampfrunden die Erleichterung um 1 erhöhen kann, bis zu dem angegebenen Maximum von 2. Dieser Vorgang erfordert Konzentration. Der Einsatz dieser Fähigkeit bedarf einer freien Aktion.");
-		final List<ParagraphData> paragraphs = Arrays.asList(pg1, pg2, pg3, pg4, pg5, pg6, pg7);
+		final ParagraphData pg7 = new ParagraphData("Um eine Modifikation anwenden zu können, muss der entsprechende Zauber mindestens 4FP haben. Für jede weitere 4FP kann eine weitere Modifikation angewendet werden.");
+		final ParagraphData pg8 = new ParagraphData("Emotionen (nur Hexe): Unter dem Einfluss zum Zauber passender Gefühle kann die Hexe eine Erleichterung von bis zu 2 für die Zauberprobe erhalten. Werden ihre Gefühle künstlich gedämpft, zum Beispiel durch Ilmenblatt, oder zaubert sie entgegen ihrer Gefühle, erschwert dies die Proben um bis zu 2. Die Hexe kann sich gezielt in ihre Emotionen hineinsteigern, sodass sie innerhalb von zwei Kampfrunden die Erleichterung um 1 erhöhen kann, bis zu dem angegebenen Maximum von 2. Dieser Vorgang erfordert Konzentration. Der Einsatz dieser Fähigkeit bedarf einer freien Aktion.");
+		final List<ParagraphData> paragraphs = Arrays.asList(pg1, pg2, pg3, pg4, pg5, pg6, pg7, pg8);
 		final ParagraphWriter writer = new ParagraphWriter(paragraphs);
 		getWriters().add(writer);
 	}
